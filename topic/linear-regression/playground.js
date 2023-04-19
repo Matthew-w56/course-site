b2a5f8efefc19f7f8b10ec722c2deeeb73ea925f
Matthew Williams <mbwilliams785@gmail.com>
2023-04-19 16:03:13 -0600

    Documented temporary Decision Tree Visualization class

--- topic/linear-regression/playground.js
+++ topic/linear-regression/playground.js
@@ -1,28 +1,58 @@
+/*
+Author: Matthew Williams
+Date: April 2023
 
+This file is meant to test the use and displaying of
+Decision Trees for the users of the site learning about
+how they work.  This is in conjunction with the now-
+finished Linear Regression add-in that allows the user
+to see Linear Regression happen in real time on a small
+set of data points that they created.
+*/
 
+//Load elements from the document
 const canvas = document.getElementById("tree-canvas");
 const ctx = canvas.getContext("2d");
+//Make the internal resolution of the canvas match it's HTML representation
+canvas.width = 550;
+canvas.height = 300;
+
+//Set some things up for the functions
 let points = [];
 //x, y, width, height, color, x/y to split next
 let sections = [[0, 0, 550, 300, 0, 0]];
 let colors = ["rgba(255, 0, 0, 0.5)", "rgba(0, 0, 255, 0.5)"];
 
-canvas.width = 550;
-canvas.height = 300;
-
+//Register events
 canvas.addEventListener("pointerdown", (e) => {
 	//Add a point
-	console.log("Registering point.");
+	console.log("Registering a new point");
 	points.push([e.offsetX, e.offsetY]);
 });
 
+/**
+ * Draws the splits make on the canvas, indicating
+ * which areas are being classified as which
+ * designation (0 or 1).
+ */
 function drawSplits() {
+	ctx.fillStyle = "rgba(255, 255, 255, 1)";
+	ctx.fillRect(0, 0, 550, 300);
 	sections.forEach((rect) => {
 		ctx.fillStyle = colors[rect[4]];
 		ctx.fillRect(rect[0], rect[1], rect[2], rect[3]);
 	});
 }
 
+/**
+ * Creates a split on the canvas at the given position.
+ * 
+ * Parameters:
+ * index		-		- (int) Index of the existing region being split
+ * split_pos	-		- (int) either the X or Y position to split the region at
+ *						  (Note: Whether it's split along X or Y depends on the region)
+ * first_designation	- (0 or 1) What the left/top section should be designated.
+ */
 function makeSplit(index, split_pos, first_designation) {
 	let rect = sections[index];
 	
@@ -74,6 +104,7 @@ function makeSplit(index, split_pos, first_designation) {
 	}
 }
 
+//Test section: runs a few splits and draws it
 makeSplit(0, 200, 0);
 makeSplit(1, 150, 1);
 makeSplit(2, 400, 0);
